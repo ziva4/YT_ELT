a56c26fa3e99673d5a1853269e1a9a6f07127c19
Swetha Arul <swetha.aetukur@gmail.com>
2026-04-19 17:54:59 +0530

    We have found unique Video Ids

--- video_stats.py
+++ video_stats.py
@@ -1,7 +1,7 @@
 import requests,json
 API_KEY = "AIzaSyAIG3_7AMrj4_uKaiISpgx14K4UWY6MkPk"
 CHANNEL_HANDLE = "MrBeast"
-
+max_results = 50
 import os 
 from dotenv import load_dotenv
 
@@ -21,5 +21,30 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+def get_video_ids(playlist_id):
+    video_ids = []
+    pageToken = None
+    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlist_id}&key={API_KEY}'
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f'&pageToken={pageToken}'
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+            pageToken = data.get('nextPageToken')
+            if not pageToken:
+                break
+        return video_ids
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlist_id = get_playlist_id() 
+    get_video_ids(playlist_id)
+
+
